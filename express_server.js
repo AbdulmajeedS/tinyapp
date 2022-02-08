8c5ac90d2160e382a383adeaa3d1b324bf2f2cf3
AbdulmajeedS <abdulmajeed.sabriye777@gmai.com>
2022-02-08 22:13:05 +0000

    updating urls and logout

--- express_server.js
+++ express_server.js
@@ -159,7 +159,7 @@ app.post("/login", (req, res) => {
 })
 
 //LOGOUT
-app.post("/urls/logout", (req, res) => {
+app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect('/urls');
 })
@@ -231,6 +231,20 @@ app.post('/urls/:shortURL/delete', (req,res) => {
   } 
 
   res.redirect('/urls');
+});
+app.post("/urls/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const newURL = req.body.newURL;
+  const loggedIn = req.session.user_id;
+
+  if (loggedIn) {
+    urlDatabase[shortURL].longURL === req.body.newURL
+    res.redirect("/urls");
+    console.log(newURL)
+
+  } else {
+    res.send("You are not authorized to edit this URL. Consider making your own ");
+  }
 });
 
 app.post('/urls/:shortURL', (req,res) => {
